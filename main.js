8b1571981ce014bcc811c622aa004c82a1c328e0
MaxumSmal37 <40183260+MaxumSmal37@users.noreply.github.com>
2021-04-21 00:37:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+
+function insert(num) {
+   document.form.textview.value = document.form.textview.value + num;
+}
+function clean() {
+   document.form.textview.value = "";
+}
+function back() {
+   var exp = document.form.textview.value;
+   document.form.textview.value = exp.substring(0, exp.length - 1);
+}
+function equal() {
+   var exp = document.form.textview.value;
+   if (exp) {
+      document.form.textview.value = eval(exp);
+   }
+}
+function percent() {
+   var exp = document.form.textview.value;
+   document.form.textview.value = exp / 100;
+}
+function sqrt() {
+   var exp = document.form.textview.value;
+   document.form.textview.value = Math.sqrt(exp)
+}
+function cbrt() {
+   var exp = document.form.textview.value;
+   document.form.textview.value = Math.cbrt(exp)
+}
+function sin() {
+   var exp = document.form.textview.value;
+   exp = exp * Math.PI / 180;
+   document.form.textview.value = Math.sin(exp)
+}
+function cos() {
+   var exp = document.form.textview.value;
+   exp = exp * Math.PI / 180;
+   document.form.textview.value = Math.cos(exp)
+}
+
+function tan() {
+   var exp = document.form.textview.value;
+   exp = exp * Math.PI / 180;
+   document.form.textview.value = Math.tan(exp)
+}
+function asin() {
+   var exp = document.form.textview.value;
+   exp = exp * Math.PI / 180;
+   document.form.textview.value = Math.asin(exp)
+}
+function acos() {
+   var exp = document.form.textview.value;
+   exp = exp * Math.PI / 180;
+   document.form.textview.value = Math.acos(exp)
+}
+
+function atan() {
+   var exp = document.form.textview.value;
+   exp = exp * Math.PI / 180;
+   document.form.textview.value = Math.atan(exp)
+}
+function rand() {
+   var exp = document.form.textview.value;
+
+   document.form.textview.value = Math.random(exp)
+}
+function PI() {
+
+   document.form.textview.value = Math.PI
+}
+function c() {
+
+   var exp = document.form.textview.value;
+   document.form.textview.value = 1/exp
+}
+
+
+addEventListener('keydown', back)
+
+
+
+
+
+
+
+
+
+
+
+
+
